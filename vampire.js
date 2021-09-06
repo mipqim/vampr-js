840ca8c07db69402eda06ec9eb3419c52bcc84cf
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-06 06:52:53 -0700

    Intro to Trees: Vampr

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let currentVampire = this;
+    let cnt = 0;
+    while (currentVampire.creator) {      
+      currentVampire = currentVampire.creator;
+      cnt++;
+    }
+    return cnt;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -37,6 +44,33 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    if (!this.creator)    return this;
+    if (!vampire.creator) return vampire;
+    if (this === vampire) return this;    
+ 
+    if (this.numberOfVampiresFromOriginal === vampire.numberOfVampiresFromOriginal) {
+      if (this.creator ===  vampire.creator) {
+        return this.creator;
+      }
+      return this.creator.closestCommonAncestor(vampire.creator);
+      
+    } else {
+      let juniorVampire;
+      let seniorVampire;
+      if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+        juniorVampire = this;
+        seniorVampire = vampire;
+      } else {
+        juniorVampire = vampire;
+        seniorVampire = this;
+      }
+
+      if (seniorVampire === juniorVampire.creator) {
+        return seniorVampire;
+      } else {
+        return seniorVampire.closestCommonAncestor(juniorVampire.creator);
+      }
+    }
   }
 }
 
